da72c7c179055868149477a0f7b36fdffd573e8c
Thiago Renan <thiagorenan1108@gmail.com>
2025-03-22 20:03:35 -0300

    User entity and resource / entidade do usuario

---
+++ src/main/java/com/devRenan01/corse/resouces/UserResoucer.java
@@ -0,0 +1,23 @@
+package com.devRenan01.corse.resouces;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devRenan01.corse.entities.User;
+
+@RestController
+// Informar que a classe é um recurso web implementando um controladosr REST
+
+@RequestMapping(value = "/users")
+// Dar nome ao recurso
+public class UserResoucer {
+	
+	// controlador rest que responde no caminho "/users"
+	@GetMapping
+	public ResponseEntity<User> findAll(){ // tipo especifico do spring para retornar respostas de requisições web
+		User u = new User(1L, "thiago", "thiago@gmail.com", "81928739","1234");
+		return ResponseEntity.ok().body(u);
+	}
+}
