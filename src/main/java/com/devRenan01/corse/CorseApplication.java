52da06df3f432b25fc812309fb7d1f8f8f2b6370
Thiago Renan <thiagorenan1108@gmail.com>
2025-03-22 19:06:58 -0300

    projet created

---
+++ src/main/java/com/devRenan01/corse/CorseApplication.java
@@ -0,0 +1,17 @@
+package com.devRenan01.corse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// anotação do spring boot para que seja uma aplicação spring boot
+
+public class CorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorseApplication.class, args);
+		//comando de rodar a aplicação do spring boot
+		
+	}
+
+}
